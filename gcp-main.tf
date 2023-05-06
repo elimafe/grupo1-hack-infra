c7c499e4136e3c9c41faad2889347ddcf2697102
elimafe <51131358+elimafe@users.noreply.github.com>
2023-05-06 13:18:01 -0300

    config molude

--- gcp-main.tf
+++ gcp-main.tf
@@ -3,7 +3,7 @@ module "sql-db" {
   version = "8.0.0"
 
   # The name of the database instance.
-  name = "module-dbhackgr1" 
+  name = "dbhackgr1" 
 
   # The region of the instance.
   region = var.region
@@ -15,11 +15,11 @@ module "sql-db" {
 
   # The deletion protection of the instance.
   deletion_protection = false
-  
+
   project_id=var.project_id
 }
 
-resource "google_artifact_registry_repository" "repo-grupo1-hack" {
+resource "google_artifact_registry_repository" "repogrupo1hack" {
   location = var.region
   repository_id = "grupo-01-384720"
   description = "Imagens Docker"
@@ -27,8 +27,8 @@ resource "google_artifact_registry_repository" "repo-grupo1-hack" {
 }
 
 # See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
-resource "google_sql_database_instance" "dbhackgr1-instance" {
-  name             = "dbhackgr1-instance"
+resource "google_sql_database_instance" "dbhackgr1instance" {
+  name             = "dbhackgr1instance"
   region           = "us-west1"
   database_version = "MYSQL_8_0"
   settings {
