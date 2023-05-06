cb3b40b6f2060387d7dce60fbd62cca6e196fe50
elimafe <51131358+elimafe@users.noreply.github.com>
2023-05-06 13:55:16 -0300

    delete repository

--- gcp-main.tf
+++ gcp-main.tf
@@ -20,14 +20,6 @@ module "sql-db" {
   project_id=var.project_id
 }
 
-resource "google_artifact_registry_repository" "repogrupo1hack" {
-  provider = google
-  location = var.region
-  repository_id = "grupo-01-384720"
-  description = "Imagens Docker"
-  format = "DOCKER"
-}
-
 # See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
 resource "google_sql_database_instance" "dbhackgr1instance" {
   name             = "dbhackgr1instance"
