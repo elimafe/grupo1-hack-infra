ef1c888ebc4698b64113f21f045b3530e6a3228c
elimafe <51131358+elimafe@users.noreply.github.com>
2023-05-06 12:28:58 -0300

    module -sql-db

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,3 +1,8 @@
+module "sql-db" {
+  source  = "GoogleCloudPlatform/sql-db/google//modules/mysql"
+  version = "8.0.0"
+}
+
 resource "google_artifact_registry_repository" "repo-grupo1-hack" {
   location = var.region
   repository_id = "grupo-01-384720"
