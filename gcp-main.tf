93e29f2a124f2f10bb4b3c6ff48b818998940e40
elimafe <51131358+elimafe@users.noreply.github.com>
2023-05-06 13:06:50 -0300

    conf module

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,3 +1,23 @@
+module "sql-db" {
+  source = "GoogleCloudPlatform/sql-db/google//modules/mysql"
+  version = "8.0.0"
+
+  # The name of the database instance.
+  name = "module-dbhackgr1"
+
+  # The machine type of the instance.
+  machine_type = "db-f1-micro"
+
+  # The region of the instance.
+  region = var.region
+
+  # The database version of the instance.
+  database_version = "MYSQL_8_0"
+
+  # The deletion protection of the instance.
+  deletion_protection = false
+}
+
 resource "google_artifact_registry_repository" "repo-grupo1-hack" {
   location = var.region
   repository_id = "grupo-01-384720"
